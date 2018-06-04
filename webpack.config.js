55ff99694a7339f62f16d96726a94e768241fec9
Cristoffer <cristoffersantoro@gmail.com>
2018-06-03 21:34:29 -0300

    Sebpack 4 React Bootstrap Starter
    Template com Webpack4, react e bootstrap configurados

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,125 @@
-const webpack =  require('webpack')
-const ExtractTextplugin = require('extract-text-webpack-plugin')
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[hash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 module.exports = {
-    entry: './src/index.jsx',
+    // mode: 'production',
+    entry: { app: './src/index.js' },
     output: {
-        path: __dirname + '/public',
-        filename: './app.js'
+        filename: '[name].[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        // name: "./images/[name].[hash].[ext]",
+                        name: '[path][name]-[hash:8].[ext]'
+                    },
+
+                },
+                {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                        },
+                        // optipng.enabled: false will disable optipng
+                        optipng: {
+                            enabled: false,
+                        },
+                        pngquant: {
+                            quality: '65-90',
+                            speed: 4
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        // the webp option will enable WEBP
+                        webp: {
+                            quality: 75
+                        }
+                    }
+                },
+                ],
+
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                minimize: true,
+                                // sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader"
+                        }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.css$/,
+                use: extractSass.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+
+        ]
     },
     devServer: {
-        port: 8080,
-        contentBase: './public'
-    },
-    resolve: {
-        extensions: ['','.js','.jsx'],
-        alias: {
-            modules: __dirname + '/node_modules'
-        }
+        contentBase: './dist',
+        hot: true,
     },
     plugins: [
-        new ExtractTextplugin('app.css')
-    ],
-    module: {
-        loaders: [{
-            test: /.js[x]?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
-            }
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextplugin.extract('style-loader','css-loader')
-        }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader: 'file'
-        }]
-    }
+        extractSass,
+        // new ExtractTextPlugin('[name].[hash].css'),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
 
-}
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+};
